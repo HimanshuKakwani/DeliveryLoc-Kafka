0126c97cf96c113d9193737a7501ea1bb88affe1
Himanshu <himanshukakwani14@gmail.com>
2024-09-11 22:46:13 +0530

    Files uploaded

---
+++ deliveryboy/deliveryboy/src/main/java/com/deliveryboy/config/AppConstants.java
@@ -0,0 +1,7 @@
+package com.deliveryboy.config;
+
+public class AppConstants {
+    // used for topic names
+    public static final String LOCATION_TOPIC_NAME = "location-update-topic";
+
+}
---
+++ deliveryboy/deliveryboy/src/main/java/com/deliveryboy/service/KafkaService.java
@@ -0,0 +1,32 @@
+package com.deliveryboy.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+// import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import com.deliveryboy.config.AppConstants;
+
+@Service
+public class KafkaService {
+
+    // kafka template used to send msgs through kafka
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    private Logger logger = LoggerFactory.getLogger(KafkaService.class);
+
+    public boolean updateLocation(String location) {
+        // kafka template sends topic and data
+        // topic made in config files under AppConstants.....why?
+        this.kafkaTemplate.send(AppConstants.LOCATION_TOPIC_NAME, location);
+        this.logger.info("Location updated..");
+        // System.out.println("-----------------------------------------");
+        return true;
+
+    }
+
+}
